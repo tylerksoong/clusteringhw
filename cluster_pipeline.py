1dd7b93abde928c1c6ec8b323466a4a20078e035
tylerksoong <soongkaisheng@gmail.com>
2026-02-05 14:36:11 -0800

    fixes

--- cluster_pipeline.py
+++ cluster_pipeline.py
@@ -1,28 +1,24 @@
 from sklearn.cluster import KMeans
-from sklearn.cluster import AgglomerativeClustering
-from sklearn.pipeline import Pipeline
 from sklearn.datasets import make_blobs
 
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-from scipy.cluster.hierarchy import dendrogram, linkage
+from scipy.cluster.hierarchy import dendrogram, linkage, fcluster
 
 
 
-X, y_true = make_blobs(n_samples=300, centers=4, cluster_std=0.60, random_state=0)
+X, _ = make_blobs(n_samples=300, centers=4, cluster_std=0.60, random_state=0)
 
 def pipeline(X, k,distance_metric="euclidean", linkage_metric="ward"): 
     k_model = KMeans(n_clusters=k)
     Z = linkage(X, method=linkage_metric, metric=distance_metric)
 
-
-
     k_model.fit(X)
 
     df = pd.DataFrame(X)
     df['cluster_labels'] = k_model.labels_
+    df['hierarchal_labels'] = fcluster(Z, t=k, criterion='maxclust')
     print(df.head())
 
     fig, axes = plt.subplots(1,2)
@@ -32,12 +28,10 @@ def pipeline(X, k,distance_metric="euclidean", linkage_metric="ward"):
     dendrogram(
         Z,
         ax=axes[1],
-
+        labels=df['hierarchal_labels']
     )
 
     plt.show()
-    print(Z)
-
     return df
 
 print(pipeline(X, 4))
