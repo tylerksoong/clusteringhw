b4729356d80b9c896c1fb23d6362d89afa57cd6b
tylerksoong <soongkaisheng@gmail.com>
2026-02-06 13:47:22 -0800

    add heatmap, tcga

--- cluster_pipeline.py
+++ cluster_pipeline.py
@@ -1,32 +1,64 @@
 from sklearn.cluster import KMeans
+from sklearn_extra.cluster import KMedoids
 from sklearn.datasets import make_blobs
+from sklearn.metrics import silhouette_score
 
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
 from scipy.cluster.hierarchy import dendrogram, linkage, fcluster
 
-X, _ = make_blobs(n_samples=300, centers=4, cluster_std=0.60, random_state=0)
-
 def pipeline(X, k,distance_metric="euclidean", linkage_metric="ward"): 
+    #perform k_means and linkage on the data set
     k_model = KMeans(n_clusters=k)
+    km_model = KMedoids(n_clusters=k)
     Z = linkage(X, method=linkage_metric, metric=distance_metric)
 
     k_model.fit(X)
+    km_model.fit(X)
 
+    #preparing output data with new cluster labels.
     df = pd.DataFrame(X)
-    df['cluster_labels'] = k_model.labels_
-    df['hierarchal_labels'] = fcluster(Z, t=k, criterion='maxclust')
-    print(df.head())
+    df['kmeans'] = k_model.labels_
+    df['kmedoids'] = km_model.labels_
+    df['hierarchal'] = np.array(fcluster(Z, t=k, criterion='maxclust')) - 1
 
-    fig, axes = plt.subplots(1,2)
+    # Convert the relevant columns to a NumPy array for speed
+    cluster_data = df[['kmeans', 'kmedoids', 'hierarchal']].sort_values('kmeans').values
 
-    axes[0].scatter(X[:,0], X[:,1], c=k_model.labels_)
+    n_samples = cluster_data.shape[0]
+    consensus_matrix = np.zeros((n_samples, n_samples))
+
+
+    for col_idx in range(cluster_data.shape[1]):
+        # by turn
+        labels = cluster_data[:, col_idx]
+        consensus_matrix += (labels[:, None] == labels[None, :])
+
+    consensus_matrix /= cluster_data.shape[1]
+
+    #Get intertia values for k 2-10
+    inertia_vals = []
+    silhouette_vals = []
+    for k in range(2,11):
+        k_model = KMeans(k)
+        k_model.fit(X)
+        inertia_vals.append(k_model.inertia_)
+        silhouette_vals.append(silhouette_score(X, k_model.labels_))
+
+
+    fig, axes = plt.subplots(1,3)
+
+    axes[0].plot(list(range(2,11)), inertia_vals)
+
     
     dendrogram(
         Z,
         ax=axes[1]
     )
+
+    axes[2].imshow(consensus_matrix)
 
     plt.show()
     return df
